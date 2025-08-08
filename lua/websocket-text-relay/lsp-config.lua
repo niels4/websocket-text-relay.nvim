710749e5164ffe8f178dc8637713c1716cbefd23
Niels Nielsen <niels.nielsen@gmail.com>
2025-08-08 04:12:43 -0600

    formatting changes only

--- lua/websocket-text-relay/lsp-config.lua
+++ lua/websocket-text-relay/lsp-config.lua
@@ -1,10 +1,10 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 local server = {
-  cmd = {"websocket-text-relay"},
-  name = "websocket-text-relay",
-  capabilities = capabilities,
-  init_options = { label = "websocket-text-relay"}
+	cmd = { "websocket-text-relay" },
+	name = "websocket-text-relay",
+	capabilities = capabilities,
+	init_options = { label = "websocket-text-relay" },
 }
 
 return server
--- lua/websocket-text-relay/simple-lsp-client.lua
+++ lua/websocket-text-relay/simple-lsp-client.lua
@@ -4,46 +4,54 @@ local client_id = nil
 local client = nil
 
 M.start = function(configIn)
-  if (client_id ~= nil) then
-    return
-  end
-  local config = vim.tbl_extend("force", configIn, {on_exit = M.on_exit})
-  local id = vim.lsp.start_client(config)
-  if not id then
-    print("ERROR: " .. config.name .. " LSP server was not able to start. Is the package installed?")
-    print("CMD: " .. vim.inspect(config.cmd))
-    return
-  end
-  client_id = id
-  client = vim.lsp.get_client_by_id(client_id)
+	if client_id ~= nil then
+		return
+	end
+	local config = vim.tbl_extend("force", configIn, { on_exit = M.on_exit })
+	local id = vim.lsp.start_client(config)
+	if not id then
+		print("ERROR: " .. config.name .. " LSP server was not able to start. Is the package installed?")
+		print("CMD: " .. vim.inspect(config.cmd))
+		return
+	end
+	client_id = id
+	client = vim.lsp.get_client_by_id(client_id)
 end
 
-M.send_file_open_notification = function (files)
-  if (client == nil) then return end
-  local params = {
-    files = files
-  }
-  client.notify("wtr/update-open-files", params)
+M.send_file_open_notification = function(files)
+	if client == nil then
+		return
+	end
+	local params = {
+		files = files,
+	}
+	client.notify("wtr/update-open-files", params)
 end
 
 M.buf_attach = function(buf)
-  if (client_id == nil) then return end
-  vim.lsp.buf_attach_client(buf, client_id)
+	if client_id == nil then
+		return
+	end
+	vim.lsp.buf_attach_client(buf, client_id)
 end
 
 M.buf_detach = function(buf)
-  if (client_id == nil) then return end
-  vim.lsp.buf_detach_client(buf, client_id)
+	if client_id == nil then
+		return
+	end
+	vim.lsp.buf_detach_client(buf, client_id)
 end
 
 M.buf_is_attached = function(buf)
-  if (client_id == nil) then return false end
-  return vim.lsp.buf_is_attached(buf, client_id)
+	if client_id == nil then
+		return false
+	end
+	return vim.lsp.buf_is_attached(buf, client_id)
 end
 
 M.on_exit = function()
-  client_id = nil
-  client = nil
+	client_id = nil
+	client = nil
 end
 
 return M
