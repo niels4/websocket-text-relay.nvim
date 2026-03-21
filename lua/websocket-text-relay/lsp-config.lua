0e51d395c96f9df190ccd51945df983faa4f7862
Niels Nielsen <niels.nielsen@gmail.com>
2026-03-21 01:28:00 -0600

    run formatter

--- lua/websocket-text-relay/lsp-config.lua
+++ lua/websocket-text-relay/lsp-config.lua
@@ -3,13 +3,13 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 local DEFAULT_UPDATES_PER_SECOND = 31
 
 local server = {
-	cmd = { "websocket-text-relay" },
-	name = "websocket-text-relay",
-	capabilities = capabilities,
-	init_options = { label = "websocket-text-relay" },
-	flags = {
-		debounce_text_changes = math.floor(1000 / DEFAULT_UPDATES_PER_SECOND),
-	},
+  cmd = { 'websocket-text-relay' },
+  name = 'websocket-text-relay',
+  capabilities = capabilities,
+  init_options = { label = 'websocket-text-relay' },
+  flags = {
+    debounce_text_changes = math.floor(1000 / DEFAULT_UPDATES_PER_SECOND),
+  },
 }
 
 return server
