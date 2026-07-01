fe14f6b44bdb331c6f75cf8cfbfb0396e6e05dbc
Niels Nielsen <niels.nielsen@gmail.com>
2026-06-30 22:52:19 -0600

    Fix issue where toggle would disable the lsp client immediately after startup

--- lua/websocket-text-relay/wtr-client.lua
+++ lua/websocket-text-relay/wtr-client.lua
@@ -3,13 +3,20 @@ local lsp_config = require('websocket-text-relay.lsp-config')
 
 local M = {}
 
+local last_enabled = 0
+local enabled = false
+
 M.enable = function()
   local config = lsp_config.get_config()
+  enabled = true
 
+  last_enabled = os.time()
   actions.start_client(config)
   vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile' }, {
     callback = function()
-      actions.start_client(config)
+      if enabled then
+        actions.start_client(config)
+      end
     end,
   })
 
@@ -17,14 +24,22 @@ M.enable = function()
 end
 
 M.disable = function()
+  enabled = false
   actions.stop_client()
   vim.notify('WTR disabled')
 end
 
 M.toggle = function()
-  if vim.lsp.is_enabled(actions.lsp_name) then
+  -- prevent toggle from disabling the server immediately after starting up
+  if os.time() - last_enabled <= 1 then
+    return
+  end
+
+  if enabled then
+    print('WTR TOggle disable')
     M.disable()
   else
+    print('WTR TOggle enable')
     M.enable()
   end
 end
